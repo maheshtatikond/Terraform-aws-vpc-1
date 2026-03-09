e610d2626aa97712a240d903f969c493049e8e1f
mahesh6419 <saimahesh6419@gmail.com>
2026-03-09 12:00:43 +1100

    actions

--- locals.tf
+++ locals.tf
@@ -1,15 +1,14 @@
 locals {
     common_tags = {
-        Project  = var.project
-        Env      = var.environment  
+        Project = var.project
+        Environment = var.environment
         Terraform = "true"
-  
-}
-    vpc_final_tags = merge (
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}"
-    },
-    var.vpc_tags
+    }
+    vpc_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.vpc_tags
     )
 }
